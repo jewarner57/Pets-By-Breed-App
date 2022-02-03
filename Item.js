7c7f698e59141708e51739b6c9f13c85f5254ab4
Jonathan Warner <jewarner57@gmail.com>
2022-02-03 15:01:32 -0500

    ffeat: display average rating of breed properties

--- Item.js
+++ Item.js
@@ -3,10 +3,26 @@ import { StyleSheet, View, Text } from 'react-native'
 
 function Item({ title, data }) {
   const properties = Object.keys(data)
+  const propertyAverage = getBreedAverageRating(data)
+
+
+  function getBreedAverageRating(data) {
+    let averageRating = 0
+    let propertyCount = 0
+    for (let rating of Object.values(data)) {
+      if (!isNaN(rating)) {
+        averageRating += rating
+        propertyCount += 1
+      }
+    }
+    averageRating /= propertyCount
+    return averageRating
+  }
 
   return (
     <View style={styles.breed}>
       <Text style={styles.info} >{title}</Text>
+      <Text style={styles.rating} >Average Rating: {isNaN(propertyAverage) ? '0' : propertyAverage.toFixed(2)}</Text>
       {properties.map((prop) => {
         return (
           prop !== 'breed' ?
@@ -34,7 +50,12 @@ const styles = StyleSheet.create({
   },
   info: {
     color: '#fff',
-    fontSize: 30,
+    fontSize: 40,
+    paddingBottom: 20,
+  },
+  rating: {
+    color: '#fff',
+    fontSize: 25,
     paddingBottom: 20,
   },
   propertyContainer: {
